076b780e228f0a8a46ec883b6759ee5551620c3d
Praveen Palani <praveen.palani89@gmail.com>
2023-03-14 08:01:27 -0400

    until cplex csvs

--- sparl-book-part2/src/ComplexCsvLoad.java
+++ sparl-book-part2/src/ComplexCsvLoad.java
@@ -1,3 +1,30 @@
+import org.apache.spark.sql.*;
+
 public class ComplexCsvLoad {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        SparkSession spk= SparkSession.builder().appName("complexCSV").master("local").getOrCreate();
+
+
+        Dataset<Row> books = spk.read().format("csv")
+        .option("header","true")
+        .option("multiline","true")
+        .option("sep",";")
+        .option("quote","*")
+        .option("dateformat","m/d/y")
+        .option("inferschema","true")
+        .load("data_files\\books.csv");
+
+        System.out.println("data");
+        books.show(7,90);
+        System.out.println("schema");
+
+        books.printSchema();
+
+
+
+
+
     
+    }   
 }
--- sparl-book-part2/src/SimpleCSVLoad.java
+++ sparl-book-part2/src/SimpleCSVLoad.java
@@ -1,5 +1,13 @@
+import org.apache.spark.sql.*;
 public class SimpleCSVLoad {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+
+
+        SparkSession spark = SparkSession.builder().appName("simple csv").master("local").getOrCreate();
+
+        Dataset<Row> books = spark.read().format("csv").option("header","true").load("data_files\\books_simple.csv");
+
+        books.show();
     }
 }
